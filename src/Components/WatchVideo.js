1dbc54a46a265b7bb7b6671c0f46a236f56aa2fc
Kitu96 <mlaxmiprava@gmail.com>
2025-11-13 15:59:15 +0530

    Created WatchPage with useSearchParam()  hook

--- src/Components/WatchVideo.js
+++ src/Components/WatchVideo.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { removeSidebar } from '../utils/navSlice';
+import { useSearchParams } from 'react-router-dom';
 
 function WatchVideo() {
     const dispatch = useDispatch();
+    const [searchParams] = useSearchParams();
+    console.log(searchParams);
     useEffect(()=>{
      dispatch(removeSidebar());
     },[])
   return (
     <div>
-      WatchPage
+      <iframe width="1200" height="600" src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+      title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+       referrerPolicy="strict-origin-when-cross-origin" 
+       allowFullScreen></iframe>
     </div>
   )
 }
