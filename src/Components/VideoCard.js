f8117718124816ff8226b3025c03267cbd4bdd1b
Kitu96 <mlaxmiprava@gmail.com>
2025-11-13 12:17:56 +0530

    Created VideoCard

--- src/Components/VideoCard.js
+++ src/Components/VideoCard.js
@@ -5,10 +5,10 @@ if(!info) return null;
  const{snippet,statistics} = info;
  const {thumbnails,channelTitle,title} = snippet;
   return (
-    <div className='p-2 flex-wrap'>
+    <div className='p-2 m-2 w-72 shadow-md'>
       <img className="rounded-lg " src={thumbnails.medium.url} alt="video-image"/>
       <ul className='w-10/12'>
-      <li className='font-bold'>{title}</li>
+      <li className='font-bold py-2'>{title}</li>
       <li>{channelTitle}</li>
       <li>{statistics.viewCount}views</li>
       </ul>      
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 
 export const GOOGLE_APIKEY="AIzaSyDjoUDcbcF3KLNGnU2HX-ptKohyQ4zvuPI"
 
-export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" + GOOGLE_APIKEY;
+export const YOUTUBE_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + GOOGLE_APIKEY;
 
 
 // 'New to you', 'Lady Gaga', 'Harshvardhan Rane', 'Jukebox', 'Arjit Singh', 'Indian Pop Music',
